3a940fce302f1c02e4999fa5dbaeb632ebc2ff36
derekkinzo <Dhak241290>
2019-07-06 13:46:33 -0400

    Added municipalities table

--- back-end/map/models.py
+++ back-end/map/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Municipality(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=64)
+    state = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
 class MalariaCase(models.Model):
-    # District where infection was first notified (diagnosed)
-    munNoti = models.IntegerField()
-    # District where patient lives
-    munResi = models.IntegerField()
-    # District where infection most likely originated
-    munInfe = models.IntegerField()
+    # Municipality where infection was first notified (diagnosed)
+    munNoti = models.ForeignKey(Municipality, on_delete=models.PROTECT, related_name="target")
+    # Municipality where patient lives
+    munResi = models.ForeignKey(Municipality, on_delete=models.PROTECT, related_name="residence")
+    # Municipality where infection most likely originated
+    munInfe = models.ForeignKey(Municipality, on_delete=models.PROTECT, related_name="origin")
 
     def __str__(self):
         return f"MUN_NOTI: {self.munNoti}, MUN_RESI: {self.munResi}, MUN_INFE: {self.munInfe}"
