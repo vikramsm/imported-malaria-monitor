17d5a10e213ca7d9407b28fa93ced6e821b3c8f2
vikramsm <vikramsm@yahoo.com>
2019-07-06 21:47:38 +0530

    additional files and changes for Heroku

--- back-end/IMM/settings.py
+++ back-end/IMM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#it$ngbkl9$-!nfgif1x1p6yn+917-dl0ukw9^!s$y7ru%9m96'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -75,13 +75,28 @@ WSGI_APPLICATION = 'IMM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
