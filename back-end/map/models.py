842d8c48f426c1e19cd90e267322fa64580373f2
derekkinzo <Dhak241290>
2019-07-06 12:55:27 -0400

    Initial add of database and MalariaCase object

--- back-end/map/models.py
+++ back-end/map/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MalariaCase(models.Model):
+    infectionDistrict = models.IntegerField()
+    patientDistrict = models.IntegerField()
+    originDistrict = models.IntegerField()
