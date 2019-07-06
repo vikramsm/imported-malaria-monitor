d00ca7759c2ba1b7bd0dc3cee85a3836fef9e2cb
derekkinzo <Dhak241290>
2019-07-06 13:27:25 -0400

    Initial add malariacase fields

--- back-end/map/models.py
+++ back-end/map/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class MalariaCase(models.Model):
-    infectionDistrict = models.IntegerField()
-    patientDistrict = models.IntegerField()
-    originDistrict = models.IntegerField()
+    # District where infection was first notified (diagnosed)
+    munNoti = models.IntegerField()
+    # District where patient lives
+    munResi = models.IntegerField()
+    # District where infection most likely originated
+    munInfe = models.IntegerField()
+
+    def __str__(self):
+        return f"MUN_NOTI: {self.munNoti}, MUN_RESI: {self.munResi}, MUN_INFE: {self.munInfe}"
