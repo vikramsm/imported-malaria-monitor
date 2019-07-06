7e5e7a6a8f2999a37a11dee0406d219c913943d5
derekkinzo <Dhak241290>
2019-07-06 12:25:26 -0400

    Created base maps app within django project

--- back-end/IMM/settings.py
+++ back-end/IMM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#it$ngbkl9$-!nfgif1x1p6yn+917-dl0ukw9^!s$y7ru%9m96'
+SECRET_KEY = '+vzl-pm1rsk3!z$(o((8alc(=b4*+6@c@$z@0%7!=-2_bu@&86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'IMM.api'
 ]
 
 MIDDLEWARE = [
