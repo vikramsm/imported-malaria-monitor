72e995c17ccbd9616d37c1dade52ed021e47c87b
derekkinzo <Dhak241290>
2019-07-06 14:57:57 -0400

    Added cross-over tables between patients and infection cases

--- back-end/map/admin.py
+++ back-end/map/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Municipality, MalariaCase, Patient
 
 # Register your models here.
+class PatientInline(admin.StackedInline):
+    model = Patient.cases.through
+    extra = 1
+
+class MalariaCaseAdmin(admin.ModelAdmin):
+    inlines = [PatientInline]
+
+class PatientAdmin(admin.ModelAdmin):
+    filter_horizontal = ("cases",)
+
 admin.site.register(Municipality)
-admin.site.register(MalariaCase)
-admin.site.register(Patient)
+admin.site.register(MalariaCase, MalariaCaseAdmin)
+admin.site.register(Patient, PatientAdmin)
