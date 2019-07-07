34778d8e9b0d03b781fdd9585344985b4f531524
Karen Dolan <karen_dolan@harvard.edu>
2019-07-07 11:33:47 -0400

    fix fom merged masters

--- back-end/map/urls.py
+++ back-end/map/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home),
-    path("<int:case_id>", views.malariacase, name="malariacase")
+    path("<int:case_id>", views.malariaCase, name="malariacase")
 ]
