fcdc38218aac96e25f49d414d646a00faf9998e0
derekkinzo <Dhak241290>
2019-07-09 12:39:47 -0400

    Commented out database reference in settings.py

--- back-end/IMM/settings.py
+++ back-end/IMM/settings.py
@@ -28,8 +28,8 @@ STATICFILES_DIRS = [(
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
