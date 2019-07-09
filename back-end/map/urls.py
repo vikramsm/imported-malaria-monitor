ec775f64a481fc2aa10e15b6f967be85155c9d77
derekkinzo <Dhak241290>
2019-07-09 15:17:15 -0400

    Added landing page

--- back-end/map/urls.py
+++ back-end/map/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.index),
+    path("map", views.home, name="map"),
     path("index", views.databaseIndex, name="databaseindex"),
     path("index/<int:case_id>", views.malariaCase, name="malariacase")
 ]
